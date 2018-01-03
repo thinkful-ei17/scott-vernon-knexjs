ad221fa3fffae609209e920e0016476c0336ae9e
Vernon Mensah <vernonmensah@gmail.com>
2018-01-03 16:22:21 -0500

    Reached 13

--- app.js
+++ app.js
@@ -64,19 +64,84 @@ process.stdout.write('\x1Bc');
 // .then(console.log);
 
 //8. Create a restaurant
-knex('restaurants').returning('id')
-.insert({
-  name: 'Slaughterhouse Five',
-  borough: 'Brooklyn',
-  cuisine: 'coffee',
-  address_building_number: '123',
-  address_street: 'Atlantic Avenue',
-  address_zipcode: '11231'
-})
-.then(console.log);
-// Destroy the connection pool
+// knex('restaurants').returning('id')
+// .insert({
+//   name: 'Slaughterhouse Five',
+//   borough: 'Brooklyn',
+//   cuisine: 'coffee',
+//   address_building_number: '123',
+//   address_street: 'Atlantic Avenue',
+//   address_zipcode: '11231'
+// })
+// .then(console.log);
+// // Destroy the connection pool
+//
+// knex.select().from('restaurants').where('id','25371').then(console.log);
 
-knex.select().from('restaurants').where('id','25371').then(console.log);
+//9. Create a restaurant and show id/name
+// knex('restaurants').returning(['name', 'id'])
+// .insert({
+//   name: 'IT',
+//   borough: 'Brooklyn',
+//   cuisine: 'coffee',
+//   address_building_number: '123',
+//   address_street: 'Clown Avenue',
+//   address_zipcode: '11231'
+// })
+// .then(console.log);
+
+// //10. Create three restaurants using a single command,
+// //with values of your choosing, returning
+// //the id and name of each restaurant.
+//
+// knex('restaurants').returning(['name', 'id'])
+// .insert([
+//   {
+//     name: 'Chucky',
+//     borough: 'Brooklyn',
+//     cuisine: 'coffee',
+//     address_building_number: '123',
+//     address_street: 'Clown Avenue',
+//     address_zipcode: '11231'
+//   },
+//   {
+//     name: 'SAW',
+//     borough: 'Brooklyn',
+//     cuisine: 'coffee',
+//     address_building_number: '123',
+//     address_street: 'Clown Avenue',
+//     address_zipcode: '11231'
+//   },
+//   {
+//     name: 'JAWS',
+//     borough: 'Brooklyn',
+//     cuisine: 'coffee',
+//     address_building_number: '123',
+//     address_street: 'Clown Avenue',
+//     address_zipcode: '11231'
+//   }
+// ])
+// .then(console.log);
+
+// //11. Update a record
+// knex('restaurants')
+// .where('nyc_restaurant_id', '30191841')
+// .update({
+//   name: 'DJ Reynolds Pub'
+// }).then(console.log);
+//
+// knex.select().from('restaurants').where('nyc_restaurant_id', '30191841').then(console.log);
+
+// //12. Delete id 10
+// knex('grades')
+// .where('id', '11')
+// .del().then(console.log);
+
+// //13. Delete id 10
+// knex('restaurants')
+// .where('id', '11')
+// .del().then(console.log);
+
 
 knex.destroy().then(() => {
   console.log('database connection closed');
